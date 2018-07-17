76ca90c044f4933fe2351237018661bdc12288ea
Alan Luu <alanluu4@gmail.com>
2018-07-17 12:06:05 -0700

    Allow JavaScript prompts in WebView

--- app/src/main/java/com/example/app/calculator/Website.java
+++ app/src/main/java/com/example/app/calculator/Website.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -128,7 +129,7 @@ public class Website {
             WebView webView = new WebView(context);
             webView.loadUrl(url);
             webView.getSettings().setJavaScriptEnabled(true);
-            webView.setWebViewClient(new WebViewClient());
+            webView.setWebChromeClient(new WebChromeClient());
             activity.setContentView(webView);
         } else {
             new AlertDialog.Builder(context).setTitle("Notice")
